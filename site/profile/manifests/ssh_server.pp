0002b6a7abe62efae18d61c3a7db0aec6c966491
steven.wang <55368622+stevenwang2019@users.noreply.github.com>
2019-10-09 12:44:39 +1100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,1 +1,16 @@
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@master.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1+f3ME/DRnaqwyltIJSSywRSIilo4cTz6nITGEsvkDje4WGxyx+71OMpEk1I3NJB9Yg4XdlDGfjfYlfwefH4HRS10XKXr2DGEXauDuZrj7T1Muqk364hzQWlzOdAVWbQKV5AgvYOlycCjn526l/AyJ3qJFYcG8qj/CgOomP+lWtNWlZBGN1oa/gDqX/D1ztCZ9n7qSVOv4YwhiWRNlIRJ5JqCvPG2eFKKuze267MsUkBy1gZf5pAhncNP5Af6D4a0p4vzhJgyn+pGsOf24jVIAaYF6uR79EwOqIccQWBhRinKN7dUXQ9sWaN1Z8EfyaPZoMH0PxzBeWUfpOvD9rvX',
+	}  
+}
 
